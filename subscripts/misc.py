05bc5b743089f2eb7858b323d8951f45a5904ecb
AcedTimo <46597640+AcedTimo@users.noreply.github.com>
2022-10-20 20:31:32 +0200

    Update misc.py
    Minor code improvements
    Added function getSubnetMask

--- subscripts/misc.py
+++ subscripts/misc.py
@@ -1,5 +1,7 @@
 import os
 import socket
+import subprocess
+
 import subscripts
 
 def clearConsole():
@@ -7,6 +9,7 @@ def clearConsole():
     if os.name in ('nt', 'dos'):
         command = 'cls'
     os.system(command)
+
 
 def printBanner():
     print("    _   __   _    __   _____")
@@ -16,6 +19,7 @@ def printBanner():
     print("/_/ |_/     |___/   /____/ - AcedTimo\n")
     return
 
+
 def getIP():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(0)
@@ -24,15 +28,32 @@ def getIP():
         s.connect(('10.254.254.254', 1))
         IP = s.getsockname()[0]
     except Exception:
-       IP = '127.0.0.1'
+        IP = '127.0.0.1'
     finally:
         s.close()
     return IP
+
+
+def getSubnetMask(ipAddress):
+    osName = os.name
+    if osName == "nt":
+        command = "powershell /c \"$mask = (Get-WmiObject Win32_NetworkAdapterConfiguration | Where IPEnabled | Select IPSubnet); $mask.IPSubnet[0]\""
+        ret = subprocess.check_output(command)
+        netmask = ipAddress + str(sum([bin(int(x)).count("1") for x in ret.decode("utf-8").split(".")]))
+    elif osName == "posix":
+        command = "ip -o -f inet addr show | awk '/scope global/ {print $4}'" # Not sure if this works yet
+        ret = subprocess.check_output(command)
+        netmask = ret.decode("utf-8")
+    else:
+        return
+
+    return netmask
 
 def getIpRange():
     ipParts = subscripts.misc.getIP().split('.')
     ipRange = ""
     for i in range(0, len(ipParts) - 1):
         ipRange += ipParts[i] + "."
-    ipRange += "0/24"
-    return ipRange
+    ipRange += "0/"
+
+    return getSubnetMask(ipRange)
